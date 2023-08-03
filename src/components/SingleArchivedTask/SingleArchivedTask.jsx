c9ebe097759b0eb772642b852319f09316c4ef49
IconicLuck <uhasib959@gmail.com>
2023-08-03 17:30:57 +0600

    implemented delete task functionality

--- src/components/SingleArchivedTask/SingleArchivedTask.jsx
+++ src/components/SingleArchivedTask/SingleArchivedTask.jsx
@@ -1,11 +1,37 @@
+import Swal from "sweetalert2";
 
 
 const SingleArchivedTask = ({ singleArchivedTask }) => {
 
     const { _id, taskTitle, taskStatus, taskDescription } = singleArchivedTask
 
-    const handleDeleteTask = (id) => {
-        console.log(id)
+    const handleDeleteTask = id => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/deleteTask/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+
     }
 
     return (
--- src/components/SingleOnHoldTask/SingleOnHoldTask.jsx
+++ src/components/SingleOnHoldTask/SingleOnHoldTask.jsx
@@ -1,8 +1,38 @@
+import Swal from "sweetalert2";
 
 
 const SingleOnHoldTask = ({ singleOnHoldTask }) => {
 
-    const { taskTitle, taskStatus, taskDescription } = singleOnHoldTask
+    const { _id, taskTitle, taskStatus, taskDescription } = singleOnHoldTask
+
+    const handleDeleteTask = id => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/deleteTask/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+
+    }
 
     return (
         <div className="card w-80 bg-base-100 shadow-xl">
@@ -12,7 +42,7 @@ const SingleOnHoldTask = ({ singleOnHoldTask }) => {
                 <p className="font-semibold text-lg">Status: {taskStatus}</p>
                 <div className="card-actions justify-end">
                     <button className="btn btn-info">Update</button>
-                    <button className="btn btn-error">Delete</button>
+                    <button onClick={() => handleDeleteTask(_id)} className="btn btn-error">Delete</button>
                 </div>
             </div>
         </div>
